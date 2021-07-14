501e775ca0c2be1aec63fb103d82ddd1d9aa0ee0
Flay <timmae2001@gmail.com>
2021-07-14 18:06:26 +0200

    initial plugin structure

---
+++ src/de/valendur/mc/ValendurMC.java
@@ -0,0 +1,37 @@
+package de.valendur.mc;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ValendurMC extends JavaPlugin implements Listener {
+	public static final String CHAT_PREFIX = ChatColor.AQUA + "Template Plugin";
+
+	private static ValendurMC plugin; 
+
+	PluginDescriptionFile pYML; 
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	public static ValendurMC getPlugin() { 
+		return plugin;
+	}
+	
+	@Override
+	public void onEnable() {
+		plugin = this;
+		PluginManager pm = getServer().getPluginManager();
+
+		//this.getCommand("example").setExecutor(new ExampleCommand());
+		//pm.registerEvents(new ExampleListener(), this);
+
+		this.getLogger()
+				.info("Yay, " + this.pYML.getName() + " is now enabled!");
+	}
+
+}
